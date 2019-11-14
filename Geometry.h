40d7b540e513dd37961e4a90b0e5313791c33fc6
Mateusz Sliwakowski <mateusz@thingtrunk.com>
2019-11-14 20:24:05 +0100

    Polish intersection calculation

--- Geometry.h
+++ Geometry.h
@@ -1,29 +1,6 @@
 #pragma once
 #include <cmath>
-
-template<class T>
-class Vector3
-{
-public:
-	constexpr Vector3() = default;
-	constexpr Vector3(T x, T y, T z) : x(x), y(y), z(z) {}
-
-	constexpr T Length() { return std::sqrt(x * x + y * y + z * z); }
-	constexpr static T Dot(Vector3<T> a, Vector3<T> b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
-
-	constexpr Vector3<T> operator* (T scalar) { return Vector3 < T(x * scalar, y * scalar, z * scalar); }
-	constexpr Vector3<T> operator/ (T scalar) { return Vector3<T>(x / scalar, y / scalar, z / scalar); }
-
-	void Normalize()
-	{
-		const auto length = Length();
-		x /= length;
-		y /= length;
-		z /= length;
-	}
-
-	T x = 0, y = 0, z = 0;
-};
+#include <tuple>
 
 template<class T>
 class Point3
@@ -39,6 +16,37 @@ public:
 };
 
 template<class T>
+class Vector3
+{
+public:
+	constexpr Vector3() = default;
+	constexpr Vector3(T x, T y, T z) : x(x), y(y), z(z) {}
+	constexpr Vector3(Point3<T> p) : x(p.x), y(p.y), z(p.z) {}
+
+	constexpr T Length() { return std::sqrt(x * x + y * y + z * z); }
+	constexpr static T Dot(Vector3<T> a, Vector3<T> b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+
+	constexpr Vector3<T> operator* (T scalar) { return Vector3 <T>(x * scalar, y * scalar, z * scalar); }
+	constexpr Vector3<T> operator/ (T scalar) { return Vector3<T>(x / scalar, y / scalar, z / scalar); }
+
+	void Normalize()
+	{
+		const auto length = Length();
+		x /= length;
+		y /= length;
+		z /= length;
+	}
+
+	T x = 0, y = 0, z = 0;
+};
+
+template<class T>
+Point3<T> operator+(Point3<T> lhs, Vector3<T> rhs)
+{
+	return Point3<T>(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+}
+
+template<class T>
 class Ray
 {
 public:
@@ -52,28 +60,29 @@ public:
 template <class T>
 class Sphere
 {
+public:
 	Sphere() = default;
 	Sphere(Point3<T> center, T radius) : C(center), R(radius) {}
 
 	std::tuple<bool, Point3<T>, Point3<T>> Intersect(Ray<T> ray)
 	{
-		const auto L = ray.C - O;
-		const auto tCa = Vector3<T>.Dot(L, D);
+		const auto L = Vector3<T>(C - ray.O);
+		auto tCa = Vector3<T>::Dot(L, ray.D);
 
 		if (tCa < 0)
 			return std::make_tuple(false, Point3<T>(), Point3<T>());
 
-		T d2 = Vector3<T>.Dot(L, L) - tCa * tCa;
+		T d2 = Vector3<T>::Dot(L, L) - tCa * tCa;
 		if (d2 > R * R)
 			return std::make_tuple(false, Point3<T>(), Point3<T>());
 
-		T thc = sqrt::(R * R - d2);
-		T t0 = tca - thc;
-		T t1 = tca + tch;
+		T thc = std::sqrt(R * R - d2);
+		T t0 = tCa - thc;
+		T t1 = tCa + thc;
 
 		auto p1 = ray.O + ray.D * t0;
 		auto p2 = ray.O + ray.D * t1;
-		return std::make_tuple(true, p1, p0);
+		return std::make_tuple(true, p1, p2);
 	}
 
 	Point3<T> C;
