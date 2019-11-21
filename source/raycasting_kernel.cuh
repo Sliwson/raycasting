c40808f852e45dcd5b8759f1e6f195f880d7c865
Mateusz Sliwakowski <mateusz@thingtrunk.com>
2019-11-21 20:46:55 +0100

    Add kernel geometric functions

--- source/raycasting_kernel.cuh
+++ source/raycasting_kernel.cuh
@@ -9,6 +9,7 @@ inline int iDivUp(int a, int b)
     return ((a % b) != 0) ? (a / b + 1) : (a / b);
 } 
 
+// Geometry
 __device__ inline void Clamp(float* value, float min = 0, float max = 1)
 {
 	if (*value < min) *value = min;
@@ -20,4 +21,39 @@ __device__ inline void ClampColor(float3* color)
 	Clamp(&color->x);
 	Clamp(&color->y);
 	Clamp(&color->z);
+}
+
+__device__ inline void TranslatePoint(float3* point, float4* cameraToWorld)
+{
+
+}
+
+__device__ inline void TranslateVector(float3* vector, float4* cameraToWorld)
+{
+
+}
+
+__device__ inline float LengthSquared(float3 vector)
+{
+
+}
+
+__device__ inline float Length(float3 vector)
+{
+
+}
+
+__device__ inline void Normalize(float3* vector)
+{
+
+}
+
+__device__ inline bool Intersect(float3 sphereOrigin, float radius, float3 rayOrigin, float3 rayDirection, float3* intersection)
+{
+
+}
+
+__device__ inline float Dot(float3 v1, float3 v2)
+{
+
 }
